0d18e198250fedc125e5e86e0fbc280d49f38e93
PrithvirajLH <chinnuhulgur@gmail.com>
2025-05-30 16:33:06 -0500

    implement companion and recent session

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,28 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async() => {
+    const companions = await getAllCompanions({limit: 3});
+    const recentSessionsCompanions = await getRecentSessions(10);
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
         <section className="home-section">
+            {companions.map(companion => (
             <CompanionCard
-                id="123"
-                name="Neura the Brainy Explorer"
-                topic="Neural Network of the Brain"
-                subject="science"
-                duration={45}
-                color="#ffda6e"
+                key={companion.id}
+                {...companion}
+                color={getSubjectColor(companion.subject)}
             />
-            <CompanionCard
-                id="456"
-                name="Countsy the Number Wizard"
-                topic="Derivatives & Integral"
-                subject="maths"
-                duration={30}
-                color="#e5d0ff"
-            />
-            <CompanionCard
-                id="789"
-                name="Verba the Vocabulary Builder"
-                topic="language"
-                subject="English Literature"
-                duration={30}
-                color="#BDE7FF"
-            />
+            ))}
         </section>
         <section className="home-section">
             <CompanionsList
                 title="Recently completed sessions"
-                companions={recentSessions}
+                companions={recentSessionsCompanions}
                 classNames="w-2/3 max-lg:w-full"
             />
             <CTA/>
--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -6,6 +6,7 @@ import {vapi} from "@/lib/vapi.sdk";
 import Image from "next/image";
 import Lottie, {LottieRef, LottieRefCurrentProps} from "lottie-react";
 import soundwaves from "@/constants/soundwaves.json"
+import {addToSessionHistory} from "@/lib/actions/companion.actions";
 enum CallStatus {
     INACTIVE = "INACTIVE",
     ACTIVE = "ACTIVE",
@@ -33,7 +34,10 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
 
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
-        const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+        const onCallEnd = () => {
+            setCallStatus(CallStatus.FINISHED);
+            addToSessionHistory(companionId)
+        }
         const onMessage = (message: Message) => {
             if(message.type === "transcript" && message.transcriptType === 'final'){
                 const newMessage = { role: message.role, content: message.transcript}
